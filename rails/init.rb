0b519d6b570135202c8263aafc540262f8b86033
Aaron Todd <aaron@thefrontiergroup.com.au>
2010-06-18 20:44:43 +0800

    Fails if DO_NOT_REQUIRE is not defined (which is outside of tests)

--- rails/init.rb
+++ rails/init.rb
@@ -2,7 +2,7 @@ if ENV['PREPARE_DB']
   Rails.configuration.after_initialize do
     puts "ENV : #{ENV['DB_PREPPED']}"      
     unless ENV['DB_PREPPED'] == "false"
-      require 'specjour/db_scrub' unless DO_NOT_REQUIRE
+      require 'specjour/db_scrub' unless defined? DO_NOT_REQUIRE
       Specjour::DbScrub.scrub
     end
   end
