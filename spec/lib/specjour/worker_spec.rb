4b0198b5aa578c591c719d0ae540340bb3b1b4a8
Aaron Todd <aaron@thefrontiergroup.com.au>
2010-06-18 20:26:34 +0800

    Added test for ENV setup at worker instantiation

--- spec/lib/specjour/worker_spec.rb
+++ spec/lib/specjour/worker_spec.rb
@@ -11,4 +11,13 @@ describe Specjour::Worker do
   it "fails" do
     boo
   end
+  
+  it "should initialise the environment variables" do
+    Specjour::Worker.new("", "", 3, "")
+    
+    ENV['PREPARE_DB'].should == 'true'
+    ENV['RSPEC_COLOR'].should == 'true'
+    ENV['TEST_ENV_NUMBER'].should == '3'
+    ENV['DB_PREPPED'].should == 'false'
+  end
 end
